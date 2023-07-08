b0803ff3082cb0af741614c0f5cef6945f2eaa9f
Joshi669 <jmaduri@hotmail.com>
2023-07-08 11:32:10 -0400

    0.5
    - Wall detection enabled with top walls and paddles
    - Ball speed fixed
    - scoreboard added to track scores

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
 
 #Setup Screen
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
-screen.title("pong")
+screen.title("Pong")
 screen.tracer(0)
 
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.penup()
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.color("white")
-paddle.goto(350, 0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle= Paddle(xcor=350, ycor=0)
+l_paddle = Paddle(xcor=-350, ycor=0)
+ball = Ball()
+score = Scoreboard()
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    #Detect Collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect Collision with Paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+
+    #Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,16 +1,19 @@
 from turtle import Turtle
 
-WIDTH = 20
-HEIGHT = 100
-X_POS = 100
-Y_POS = 0
-
-
 class Paddle(Turtle):
-    def __init__(self):
+    
+    def __init__(self, xcor, ycor):
+        super().__init__()
         self.shape("square")
-        self.color("white")
-        self.shapesize(stretch_wid=WIDTH, stretch_len=HEIGHT)
         self.penup()
-        self.goto(X_POS, Y_POS)
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.goto(xcor, ycor)
 
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
